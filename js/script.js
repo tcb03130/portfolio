5df6e5300eda1fa36194a5fc6dfaf756c149605a
tcb03130 <tcb03130@gmail.com>
2024-08-13 14:15:11 +0900

    포트폴리오

---
+++ js/script.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const video = document.getElementById("myVideo");
+  if (video) {
+    video.setAttribute("preload", "auto");
+  }
+});
+
+window.addEventListener('scroll', () => {
+  const scrollToTop = document.querySelector('.scroll-to-top');
+  if (scrollToTop) {
+    scrollToTop.classList.toggle('show', window.scrollY > 200);
+  }
+});
+
+document.querySelector('.scroll-to-top')?.addEventListener('click', (event) => {
+  event.preventDefault();
+  
+  function scrollToTop() {
+    let currentPosition = window.pageYOffset;
+    if (currentPosition > 0) {
+      window.requestAnimationFrame(scrollToTop);
+      window.scrollTo(0, currentPosition - currentPosition / 8);
+    }
+  }
+  
+  scrollToTop();
+});
+
+document.addEventListener('mousemove', (e) => {
+  const cursor = document.querySelector('.cursor');
+  if (cursor) {
+    const mouseX = e.pageX + 10; // 커서 이미지의 x좌표
+    const mouseY = e.pageY + 10; // 커서 이미지의 y좌표
+    cursor.style.left = `${mouseX}px`;
+    cursor.style.top = `${mouseY}px`;
+  }
+});
