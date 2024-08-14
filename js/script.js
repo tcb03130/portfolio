066dcf7ae167a74c64066e0feb947515b54db242
tcb03130 <tcb03130@gmail.com>
2024-08-14 10:36:51 +0900

    제발 되라

--- js/script.js
+++ js/script.js
@@ -5,16 +5,16 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
-window.addEventListener('scroll', () => {
-  const scrollToTop = document.querySelector('.scroll-to-top');
+window.addEventListener("scroll", () => {
+  const scrollToTop = document.querySelector(".scroll-to-top");
   if (scrollToTop) {
-    scrollToTop.classList.toggle('show', window.scrollY > 200);
+    scrollToTop.classList.toggle("show", window.scrollY > 200);
   }
 });
 
-document.querySelector('.scroll-to-top')?.addEventListener('click', (event) => {
+document.querySelector(".scroll-to-top")?.addEventListener("click", (event) => {
   event.preventDefault();
-  
+
   function scrollToTop() {
     let currentPosition = window.pageYOffset;
     if (currentPosition > 0) {
@@ -22,12 +22,12 @@ document.querySelector('.scroll-to-top')?.addEventListener('click', (event) => {
       window.scrollTo(0, currentPosition - currentPosition / 8);
     }
   }
-  
+
   scrollToTop();
 });
 
-document.addEventListener('mousemove', (e) => {
-  const cursor = document.querySelector('.cursor');
+document.addEventListener("mousemove", (e) => {
+  const cursor = document.querySelector(".cursor");
   if (cursor) {
     const mouseX = e.pageX + 10; // 커서 이미지의 x좌표
     const mouseY = e.pageY + 10; // 커서 이미지의 y좌표
@@ -35,3 +35,16 @@ document.addEventListener('mousemove', (e) => {
     cursor.style.top = `${mouseY}px`;
   }
 });
+
+function toggleImage() {
+  var img = document.getElementById("image");
+  var button = document.querySelector("button");
+
+  if (img.style.display === "none") {
+    img.style.display = "block"; // 이미지 보이기
+    button.textContent = "이미지 숨기기"; // 버튼 텍스트 변경
+  } else {
+    img.style.display = "none"; // 이미지 숨기기
+    button.textContent = "이미지 보이기"; // 버튼 텍스트 변경
+  }
+}
